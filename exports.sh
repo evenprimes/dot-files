9f113190a80be97deaf3c08179ede1eda63ba557
Chris Cioffi <chris@evenprimes.com>
2015-12-20 16:46:59 -0500

    Installed completion into /use/local/etc and that picked up git-prompt and git-completion.bash from Brew.

--- exports.sh
+++ exports.sh
@@ -1,6 +1,3 @@
 export EDITOR='subl -n -w'
-
-# export DIR_COLORS=~/.dircolors
-
 export WORKON_HOME=~/Source
 export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ files="aliases.sh bash_prompt.sh dircolors exports.sh functions.sh git-prompt gi
 ##########
 # Update dircolors
 gdircolors dircolors.moonshine > dircolors
+
 # move any existing dotfiles in homedir to dotfiles_old directory, then
 # create symlinks from the homedir to any files in the ~/dotfiles
 # directory specified in $files
@@ -15,11 +16,8 @@ for file in $files; do
 	sourceFile=$dir/$file
 	destFile=~/.$file
 
-	# echo $sourceFile
-	# echo $destFile
 	if [ ! -L $destFile ]; then
 		echo "Creating symlink to $file in home directory."
 		ln -sv $sourceFile $destFile
 	fi
 done
-
