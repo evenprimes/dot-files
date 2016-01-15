3531db491025bfeec915b3e6efbd6109eb36c21b
Chris Cioffi <chris@evenprimes.com>
2016-01-15 17:55:06 -0500

    Change my pager to most

--- exports.sh
+++ exports.sh
@@ -1,4 +1,8 @@
+# make sublime text the default editor
 export EDITOR='subl -n -w'
+
+# use most as the pager
+export PAGER="most"
 
 # Python virtualenv stuff
 export WORKON_HOME=~/Source/.virtualenvs
