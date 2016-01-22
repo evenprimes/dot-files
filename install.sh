a2d1cf7dc909f5e2d7cac714c820190e24a6b10b
Chris Cioffi <chris@evenprimes.com>
2016-01-22 11:41:21 -0500

    Removed CDPATH from exports.sh and restructured install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,30 @@
 #!/usr/bin/env bash
 
-########## Variables
+main() {
+	########## Variables
 
-dir=$(pwd)                     # dotfiles directory
-files="aliases.sh bash_prompt.sh dircolors exports.sh functions.sh git-prompt git-completion.bash gitconfig gitignore_global profile vimrc"
+	readonly local dir=$(pwd)                     # dotfiles directory
+	readonly local files="aliases.sh bash_prompt.sh bashrc dircolors exports.sh functions.sh gitconfig gitignore_global profile vimrc"
+	local i=
+	local sourceFile=
+	local destFile=
 
-##########
-# Update dircolors
-gdircolors dircolors.moonshine > dircolors
+	##########
+	# Update dircolors
+	gdircolors dircolors.moonshine > dircolors
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then
-# create symlinks from the homedir to any files in the ~/dotfiles
-# directory specified in $files
-for file in $files; do
-	sourceFile=$dir/$file
-	destFile=~/.$file
+	# move any existing dotfiles in homedir to dotfiles_old directory, then
+	# create symlinks from the homedir to any files in the ~/dotfiles
+	# directory specified in $files
+	for i in $files; do
+		sourceFile=$dir/$i
+		destFile=~/.$i
 
-	if [ ! -L $destFile ]; then
-		echo "Creating symlink to $file in home directory."
-		ln -sv $sourceFile $destFile
-	fi
-done
+		if [ ! -L $destFile ]; then
+			echo "Creating symlink to $i in home directory."
+			ln -sv $sourceFile $destFile
+		fi
+	done
+}
+
+main
