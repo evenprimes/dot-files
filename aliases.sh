0758c2faf0dbcc9a978efb85551563cf94e9371d
Chris Cioffi <chris@evenprimes.com>
2016-01-30 13:59:50 -0500

    Add a few new aliases

--- aliases.sh
+++ aliases.sh
@@ -4,6 +4,7 @@ alias ll='gls --color=auto -l --almost-all --group-directories-first --classify 
 alias l.='gls --color=auto --directory --group-directories-first --classify .*'
 alias ls='gls -p --classify --color=auto --group-directories-first --ignore-backups'
 alias ..='cd ..'
+alias cd..='cd ..'
 
 alias gs='git status '
 alias ga='git add '
@@ -18,6 +19,7 @@ alias c='clear'
 alias mv='mv -i'
 alias cp='cp -i'
 alias ln='ln -i'
+alias rm='rm -I --preserve-root --dir'
 
 alias df='df -H'
 alias du='du -ch'
@@ -28,3 +30,5 @@ alias ax='chmod a+x'
 
 # copy the working directory path
 alias cpwd='pwd|tr -d "\n"|pbcopy'
+
+alias ping='ping -c 5'
