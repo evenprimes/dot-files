4a3e5fe1d7eb487435d12c0f21454f346bc48be9
Chris Cioffi <chris@evenprimes.com>
2016-02-04 10:58:00 -0500

    Comment out unused colors.

--- bash_prompt.sh
+++ bash_prompt.sh
@@ -76,12 +76,12 @@ if tput setaf 1 &> /dev/null; then
 	bold=$(tput bold);
 	reset=$(tput sgr0);
 	# Solarized colors, taken from http://git.io/solarized-colors.
-	black=$(tput setaf 0);
+	# black=$(tput setaf 0);
 	blue=$(tput setaf 33);
-	cyan=$(tput setaf 37);
+	# cyan=$(tput setaf 37);
 	green=$(tput setaf 64);
 	orange=$(tput setaf 166);
-	purple=$(tput setaf 125);
+	# purple=$(tput setaf 125);
 	red=$(tput setaf 124);
 	violet=$(tput setaf 61);
 	white=$(tput setaf 15);
@@ -89,12 +89,12 @@ if tput setaf 1 &> /dev/null; then
 else
 	bold='';
 	reset="\e[0m";
-	black="\e[1;30m";
+	# black="\e[1;30m";
 	blue="\e[1;34m";
-	cyan="\e[1;36m";
+	# cyan="\e[1;36m";
 	green="\e[1;32m";
 	orange="\e[1;33m";
-	purple="\e[1;35m";
+	# purple="\e[1;35m";
 	red="\e[1;31m";
 	violet="\e[1;35m";
 	white="\e[1;37m";
